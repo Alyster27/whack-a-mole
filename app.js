1ae4d47fbae89fac517189821a67f054d68ff2a7
Ali <59686248+Alyster27@users.noreply.github.com>
2022-06-08 15:10:30 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const squares = document.querySelectorAll('.square');
+const mole = document.querySelector('.mole');
+const timeLeft = document.getElementById('timer');
+const score = document.querySelector('#score');
+
+let result = 0;
+let hitPosition
+let currentTime = 10;
+let timerid = null;
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole');
+    });
+
+    let randomSquare = squares[Math.floor(Math.random() * squares.length)];
+randomSquare.classList.add('mole');
+
+hitPosition = randomSquare.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+    if (square.id == hitPosition) {
+        result++;
+        score.textContent = result;
+        hitPosition = null;
+    }
+    });
+});
+
+function moveMole() {
+    
+    timmerId = setInterval(randomSquare, 500);
+}
+
+
+moveMole();
+
+function countDown () {
+    currentTime--;
+    timeLeft.textContent = currentTime
+
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId);
+        clearInterval(timmerId);
+        alert('Game Over! Your score is ' + result);
+    }
+};
+
+let countDownTimerId = setInterval(countDown, 1000);
